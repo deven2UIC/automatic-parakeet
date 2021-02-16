2d6934e29b25300e7632b743ed54d7fe943b25c1
deven2UIC <70229259+deven2UIC@users.noreply.github.com>
2021-02-16 10:10:49 -0600

    Done

--- HW4/Python/main.py
+++ HW4/Python/main.py
@@ -7,6 +7,31 @@ print(lines)
 
 instr_dict = {}
 label_dict = {}
+
+type_dict = {}
+type_dict['add'] =  'r'
+type_dict['slt'] =  'r'
+type_dict['addi'] = 'i'
+type_dict['ori'] =  'i'
+type_dict['lw'] =   'i'
+type_dict['sw'] =   'i'
+type_dict['beq'] =  'i'
+
+# R-type only
+funct_dict = {}
+funct_dict['add'] =  0b100000
+funct_dict['slt'] =  0b101010
+
+# All types
+op_dict = {}
+op_dict['add'] =  0b000000
+op_dict['slt'] =  0b000000
+op_dict['addi'] = 0b001000
+op_dict['ori'] =  0b001101
+op_dict['lw'] =   0b100011
+op_dict['sw'] =   0b101011
+op_dict['beq'] =  0b000100
+
 # PC : instr
 PC = 0
 for ln in lines:
@@ -39,33 +64,71 @@ for PC, instr in instr_dict.items():
     func = instr.split()
     func[1] = func[1].replace('$', '')
     func[1] = func[1].replace(',', '')
-
     func[2] = func[2].replace('$', '')
     func[2] = func[2].replace(',', '')
+    #print(instr)
 
-    print(func)
+    op = op_dict[func[0]]
 
-    if ( func[0] == 'add'):        # R-type instructions
-        print(func)
-        op = 0
-        rd = func[1]
-        rs = func[2]
-        rt = func[3]
+    if ( type_dict[ func[0] ] == 'r'):        # R-type instructions
+        func[3] = func[3].replace('$', '')
+        func[3] = func[3].replace(',', '')
 
+        rd = int(func[1], base=10)
+        rd = rd << 11
 
-    elif (func[0] == 'slt'):
-        print(func)
-    elif ( func[0] == 'addi'):     # I-type instructions
-        print(func)
-    elif ( func[0] == 'ori'):
-        print(func)
-    elif ( func[0] == 'lw'):
-        print(func)
-    elif ( func[0] == 'sw'):
-        print(func)
-    elif ( func[0] == 'beq'):
-        print(func)
+        rs = int(func[2], base=10)
+        rs = rs << 21
 
-    print('\n')
+        rt = int(func[3], base=10)
+        rt = rt << 16
+
+        inst_bin = (op << 26) + rs + rt + rd + funct_dict[func[0]]
+        print('PC = {0}:  0x{1:08x}'.format(PC, inst_bin))
+    else:                                   # I-type instructions
+        if (func[0] == 'sw' or func[0] == 'lw'): # sw/lw
+            rt = int(func[1], base=10)
+            add = func[2].split('(')
+            offset = add[0]
+            base = add[1]
+
+            if (offset.find('0x') != -1):
+                offset.replace('0x', '')
+                offset = int(offset, base=16)
+            else:
+                offset = int(offset, base=10)
+
+            base = base.replace(')', '')
+            base = int(base, base=10)
+
+            inst_bin = (op << 26) + (base << 21) + (rt << 16) + offset
+            print('PC = {0}:  0x{1:08x}'.format(PC, inst_bin))
+
+        elif ( func[0] == 'beq'):
+            inst_bin = 0
+            rs = int(func[1], base=10)
+            rt = int(func[2], base=10)
+            label = func[3]
+            offset = (label_dict[label] - (PC + 4)) >> 2
+
+            inst_bin = (op << 26) + (rs << 21) + (rt << 26) + offset
+            print('PC = {0}:  0x{1:08x}'.format(PC, inst_bin))
+
+        else: # all other I-types
+            rt = int(func[1], base=10)
+            rs = int(func[2], base=10)
+
+            imm = func[3]
+            if (imm.find('0x') != -1):
+                imm.replace('0x', '')
+                imm_b = int(imm, base=16)
+            else:
+                imm_b = int(imm, base=10)
+
+            inst_bin = (op << 26) + (rs << 21) + (rt << 16) + imm_b
+            print('PC = {0}:  0x{1:08x}'.format(PC, inst_bin))
+
+    #print('')
+
 
 
